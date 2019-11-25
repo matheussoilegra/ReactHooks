205b101abb1b5d376827f37caf630c9300df317d
Matheus Santos <matheus.santos@ilegra.com>
2019-11-25 11:09:23 -0300

    Added useSted hook.
    Generated random ids.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Almost Home', id: 1 },
+    { title: 'Memory Gospel', id: 2 },
+    { title: 'This Wild Darkness', id: 3 }
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { title: 'new song', id: uuid() }]);
+  };
+
   return (
     <div className="song-list">
       <ul>
-        <li>This Wild Darkness</li>
-        <li>Memory Gospel</li>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 };
